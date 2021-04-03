392491d442a25fcb5a01d2cd564c6acd4ac4fce3
Praveen Kumar Gnanavel <77199568+A00443487@users.noreply.github.com>
2021-04-04 00:07:24 +0530

    database connection made

---
+++ HRP/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRP.Models
+{
+    public class User
+    {
+        //public int id { get; set; }
+        [Key]
+        public int sin { get; set; }
+
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string contact { get; set; }
+        public char gender { get; set; }
+
+        public DateTime dob { get; set; }
+
+        public int address_id { get; set; }
+        public string is_police { get; set; }
+
+    }
+}
