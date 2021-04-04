d5de1a28667b1b31edaeefb61b09ef746da6ccb9
A00443487 <praveen.kumar.gnanavel@smu.ca>
2021-04-04 23:20:07 +0530

    Front Code update
    Hi Aditi,
    
    Kinldy work on Front work and try to complete it before moday EOD. Thank you.
    
    Regards,
    Praveen Kumar Gnanavel

---
+++ HRP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HRP/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRP.Models
+{
+    public class User
+    {
+        //public int id { get; set; }
+        [Key]
+        [Required]
+        [Display(Name = "SIN")]
+        public int sin { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string fname { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lname { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string password { get; set; }
+        [Required]
+        [Display(Name = "Contact")]
+        public string contact { get; set; }
+        [Required]
+        [Display(Name = "Gender")]
+        public char gender { get; set; }
+        [Required]
+        [Display(Name = "Date Of Birth")]
+        public DateTime dob { get; set; }
+
+        public int address_id { get; set; }
+        public string is_police { get; set; }
+
+    }
+}
