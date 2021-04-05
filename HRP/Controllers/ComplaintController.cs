829d0ed061e381ecf3296227277b899fd85c603a
Kishor Deshmukh <kishor.deshmukh@smu.ca>
2021-04-05 11:17:37 -0300

    Create a controller for Complaint Form.

---
+++ HRP/Controllers/ComplaintController.cs
@@ -0,0 +1,25 @@
+﻿using HRP.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRP.Controllers
+{
+    public class ComplaintController : Controller
+    {
+        private HrpDbContext obj_hrpdbcontext;
+
+
+        public ComplaintController(HrpDbContext hrpdbcontext)
+        {
+            obj_hrpdbcontext = hrpdbcontext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
