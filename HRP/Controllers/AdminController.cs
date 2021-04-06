fe828c49989ae241beca4732cad1e3d68aeff541
A00443487 <praveen.kumar.gnanavel@smu.ca>
2021-04-06 13:29:46 +0530

    Position Management Updated

--- HRP/Controllers/AdminController.cs
+++ HRP/Controllers/AdminController.cs
@@ -120,7 +120,90 @@ namespace HRP.Controllers
             return RedirectToAction("PoliceList");
 
         }
+        public IActionResult PositionList()
+        {
 
+            return View(obj_hrpdbcontext.Positions);
+        }
+        public IActionResult PositionCreate()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PositionCreated(Position position)
+        {
+            
+            try
+            {
+                int exist = obj_hrpdbcontext.Positions.Count(x => x.name == position.name);
+                if (exist > 0)
+                {
+                    throw new DbUpdateException();
+                }
+                else
+                {
+                    obj_hrpdbcontext.Positions.Add(position);
+                    obj_hrpdbcontext.SaveChanges();
+                }
+                
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction("IdExist");
+            }
+            return RedirectToAction("PositionList");
+
+        }
+
+        [HttpPost]
+        public IActionResult PositionDelete(int id)
+        {
+            var position = obj_hrpdbcontext.Positions.Where(data => data.id == id).FirstOrDefault();
+            obj_hrpdbcontext.Positions.Remove(position);
+            obj_hrpdbcontext.SaveChanges();
+            return RedirectToAction("PositionList");
+        }
+
+        //PENDING
+        public IActionResult PositionUpdate(int id)
+        {
+            
+            
+
+            Position position = obj_hrpdbcontext.Positions.Where(data => data.id == id).FirstOrDefault();
+            
+            return View(position);
+        }
+
+        [HttpPost]
+
+        public IActionResult PositionUpdated(int sin, string position, int activation_code)
+        {
+            var sin1 = sin;
+
+            Police police = new Police();
+            var data = obj_hrpdbcontext.Positions.ToList().Where(x => x.name == position);
+            foreach (Position d in data)
+            {
+                police.position_id = d.id;
+            }
+
+            police.sin = sin;
+            police.activation_code = Convert.ToString(activation_code);
+            try
+            {
+                obj_hrpdbcontext.Polices.Update(police);
+                obj_hrpdbcontext.SaveChanges();
+            }
+            catch (DbUpdateException dbexception)
+            {
+                return RedirectToAction("IdExist");
+            }
+            return RedirectToAction("PoliceList");
+
+        }
 
 
 
