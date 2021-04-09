39fcc29cdd580d27953a16289b610ab1bb58b188
Kishor Deshmukh <kishor.deshmukh@smu.ca>
2021-04-09 16:23:55 -0300

    Added routing.

--- HRP/Controllers/RegisterController.cs
+++ HRP/Controllers/RegisterController.cs
@@ -16,6 +16,8 @@ namespace HRP.Controllers
         {
             obj_HrpDbContext = hrp;
         }
+        
+        [Route("Police-Registration")]
         public IActionResult Police()
         {
             return View();
@@ -66,10 +68,14 @@ namespace HRP.Controllers
             
         }
 
+
+        [Route("Civilian-Registration")]
+
         public IActionResult Civilian()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Civilian(RegisterVM rVM)
         {
